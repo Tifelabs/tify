f238997f44b4824ff0ee4ec73a438539dd987935
Tifelabs <boluwatifeolusegun018@gmail.com>
2024-07-03 13:01:27 -0300

    fuck it , I pushed it successfully

---
+++ Readme.md
@@ -0,0 +1,16 @@
+### Hello I'm <font color="blue">Tife</font> 👋
+<p align="center">
+I am a Backend Software Dev
+</p>
+
+<p align="center">
+ I’m currently working on JavaScript and Bash...
+</p>
+
+<p align="center">
+⌜ I am excited about learning new things ⌟
+</p>
+
+<p align="center">
+════ ⋆★⋆ ════
+</p>
---
+++ public/index.js
@@ -0,0 +1,16 @@
+//Clock Stuff
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = currentTime;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // initial call to display clock immediately
+
+
+//Read more
