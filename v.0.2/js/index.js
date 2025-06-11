45764969e9094bd420b6d3fff20dd2de171c425f
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-06-11 20:00:52 -0300

    wip : Add theme

--- v.0.2/js/index.js
+++ v.0.2/js/index.js
@@ -30,3 +30,20 @@ document.addEventListener('DOMContentLoaded', () => {
             alert("Thank you for your interest in donating!You'll be redirected to the donation page.");
             window.location.href = "https://ko-fi.com/tifelabs";
 }
+
+//THEME STUFF
+function changeTheme() {
+    const theme = document.getElementById('style').value;
+    document.body.className = ''; // Reset classes
+    document.body.classList.add(theme);
+    localStorage.setItem('selectedTheme', theme); // Save theme
+}
+
+// Apply saved theme on page load
+window.onload = function() {
+const savedTheme = localStorage.getItem('selectedTheme');
+if (savedTheme) {
+    document.getElementById('style').value = savedTheme;
+    document.body.classList.add(savedTheme);
+   }
+};
