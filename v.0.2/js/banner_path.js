6581240467cf8a557e20e17beb8483cc13562b03
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-06-13 21:21:32 -0300

    ::include banner path for main page

---
+++ v.0.2/js/banner_path.js
@@ -0,0 +1,13 @@
+window.bannerImages = [
+        "./v.0.2/assets/banner/earth.jpg",
+        "./v.0.2/assets/banner/planets.jpg",
+        "./v.0.2/assets/banner/aya.jpg",
+        "./v.0.2/assets/banner/rd.png",
+        "./v.0.2/assets/banner/sa-h.jpg",
+        "./v.0.2/assets/banner/sa.jpg",
+        "./v.0.2/assets/banner/spike.jpg",
+        "./v.0.2/assets/banner/nice.jpg",
+        "./v.0.2/assets/banner/Emotion.png",
+        "./v.0.2/assets/banner/pirate.gif",
+        "./v.0.2/assets/banner/ps2.gif"
+];
