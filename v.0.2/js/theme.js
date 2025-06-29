94d4a13a68b7b1a3657e6ada7590c1fd5aa0c193
Tifelabs <boluwatifeolusegun018@gmail.com>
2025-06-29 14:52:54 -0300

    Fix :: margin and type case

--- v.0.2/js/theme.js
+++ v.0.2/js/theme.js
@@ -1,18 +1,18 @@
-        function changeTheme() {
-            const theme = document.getElementById('style').value;
-            document.body.className = ''; // Reset classes
-            document.body.classList.add(theme);
-            localStorage.setItem('selectedTheme', theme); // Save theme
-        }
+function changeTheme() {
+    const theme = document.getElementById('style').value;
+    document.body.className = ''; // Reset classes
+    document.body.classList.add(theme);
+    localStorage.setItem('selectedTheme', theme); // Save theme
+}
 
-        // Apply saved theme on page load
-        window.onload = function() {
-            const savedTheme = localStorage.getItem('selectedTheme');
-            if (savedTheme) {
-                const themeSelect = document.getElementById('style');
-                if (themeSelect) {
-                    themeSelect.value = savedTheme;
-                }
-                document.body.classList.add(savedTheme);
+// Apply saved theme on page load
+window.onload = function() {
+    const savedTheme = localStorage.getItem('selectedTheme');
+        if (savedTheme) {
+            const themeSelect = document.getElementById('style');
+            if (themeSelect) {
+                themeSelect.value = savedTheme;
             }
-        };
+            document.body.classList.add(savedTheme);
+    }
+};
